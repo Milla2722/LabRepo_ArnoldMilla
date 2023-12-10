5233ae47e0d7b78584f41ba9ea9517de102b8840
Milla2722 <147879029+Milla2722@users.noreply.github.com>
2023-12-09 22:50:06 -0600

    Add files via upload

--- LabRepo_ArnoldMilla/src/labrepo_arnoldmilla/LabRepo_ArnoldMilla.java
+++ LabRepo_ArnoldMilla/src/labrepo_arnoldmilla/LabRepo_ArnoldMilla.java
@@ -49,14 +49,21 @@ static Random rand = new Random();
                         while (seguir == 's' || seguir == 'S'){
                            int dado = rand.nextInt(1,17);
                             System.out.println("Usted saco: " + dado);
+                            ///////////BUFFS
                             ArrayList<Integer> datos= datos(dado,tablero,personaje);
                             int buff = datos.get(0);
                             if (buff == 5){
+                                System.out.println("Encontro un cofre con 5 de energia!");
                                 energy = energy + buff;
                             }
                             else if(buff == 20){
+                                System.out.println("Encontro un cofre con 20 de vida!");
                                 hp = hp + buff;
                             }
+                            else{
+                                System.out.println("No encontro ningun cofre");
+                            }
+                            ///BUFFs
                             System.out.println("Puntos de vida: " + hp);
                             System.out.println("Puntos de energia: " + energy);
                             System.out.println("Ataque: 50");
@@ -128,6 +135,7 @@ static Random rand = new Random();
         ArrayList<Integer> datos = new ArrayList<Integer>();
         int dado1 = 0;
         int dado2 = 0;
+        int cont = 0;
         
         ///calculo para los dados
         if (dado > 9){
@@ -139,8 +147,8 @@ static Random rand = new Random();
         }
             
         
-        for (int cont = 0; cont < 10; cont++) {
-            for (int contador = 0; contador < 10; contador++) {
+        for (cont = 0; cont < tablero.length; cont++) {
+            for (int contador = 0; contador < tablero[cont].length; contador++) {
                 ///no se porque diablos siempre comienza desde 0 pero bueno
                 if (tablero [cont][contador] == personaje){
                     tablero [cont][contador] = '-';
@@ -157,7 +165,8 @@ static Random rand = new Random();
                     break;
                 }
             }
-            if (dado > 9){
+
+            if (dado > 9){                
                 if(tablero[cont+1][dado1-1] == '♥'){
                     datos.add(20);  
                     }
@@ -166,22 +175,41 @@ static Random rand = new Random();
                 }
                 //lo mismo acá, y por tal razon no puedo bajar del primer nivel 
                 //y como no encuentra una c o m en la primer fila luego no la reemplaza arriba
-                datos.add(cont + 1);
+                datos.add(cont + 1); // esto no lo ocupo, pero si no lo tiene no funciona el codigo
                 tablero[cont][dado2] = '-';
                 tablero[cont + 1][dado1 - 1] = '-';
                 tablero [cont + 1][dado1 - 1] = personaje; 
                 break;
             }
             else{
-                datos.add(cont + 1);
+                datos.add(0);
+                datos.add(cont + 1); //igual con esto
                 break;
             }
         }
         imprimirT(tablero);
+        dragons(cont,dado);
         return datos;
     }
     
-    //////////////////////funcion para crear el laberinto con for tambien se puede hacer como el que está arriba
+    ///////dragones
+    public static void dragons (int cont,int dado){
+        int prob = rand.nextInt(0,101);
+        if (dado % 2 == 1){
+            if (cont == 0 || cont == 1 || cont == 2){
+                int cant = 2;
+                System.out.println("-------Encontro " + cant + " dragones--------");
+                if (prob > 45){
+                    System.out.println("Encontro dos dragones y les ganó");
+                }
+                else {
+                    System.out.println("Encontro dos dragones y perdio, pierde 50 de vida");
+                }
+            }
+        }
+    }
+    
+    //////////////////////funcion para crear el laberinto con for tambien se puede hacer como la funcion inicializar
     public static char [][] creator(){
         char matriz [][] = new char [8][10];
         
